3017e2636794bf90cef202c7af3384b3e6eceb12
Marcelo Rosa <omarcelorosa3@gmail.com>
2026-03-22 21:39:22 -0300

    add questao6
    add questao6 and update questao2

--- estatistica/questao2.py
+++ estatistica/questao2.py
@@ -1,15 +1,15 @@
 while True:
+    fracao1 = input("Digite a primeira fração (ex: 1/2): ")
+    numerador1, denominador1 = fracao1.split("/") #quebra em string e atribui valores as variáveis
+    fracao2 = input("Digite a segundafração (ex: 1/2): ")
+    numerador2, denominador2 = fracao2.split("/")
+    
     operacao= input("Digite a operação (+, -, *, /) ou (sair): ")
 
     if operacao == "sair":
         print("Encerrando o cálculo.")
         break
-   
-    fracao1 = input("Digite a primeira fração (ex: 1/2): ")
-    numerador1, denominador1 = fracao1.split("/") #quebra em string e atribui valores as variáveis
-    fracao2 = input("Digite a segundafração (ex: 1/2): ")
-    numerador2, denominador2 = fracao2.split("/")
-        
+      
     #conversão de string para inteiro
     n1 = int(numerador1)
     d1 = int(denominador1)
@@ -20,24 +20,32 @@ while True:
         case "+":
             numerador = ((n1 * d2) + (n2 * d1))
             denominador = (d1 * d2)
-            resultado = f"{numerador}/{denominador}"
         case "-":
             numerador = ((n1 * d2) - (n2 * d1))
             denominador = (d1 * d2)
-            resultado = f"{numerador}/{denominador}"
         case "*":
             numerador = (n1 * n2)
             denominador = (d1 * d2)
-            resultado = f"{numerador}/{denominador}"
         case "/":
             if n2 == 0:
                 print("Erro: Divisão por zero não é permitida.")
                 exit()
             numerador = (n1 * d2)
-            denominador = (d1 * n2)
-            resultado = f"{numerador}/{denominador}"
+            denominador = (d1 * n2)            
         case _: 
             print("Operação inválida!")
             exit()
+    
+    import math
 
-    print("Resultado:", f"{numerador}/{denominador}")
+    divisor = math.gcd(numerador, denominador) #mmc
+    numerador //= divisor #divisão inteira para simplificar a fração
+    denominador //= divisor
+
+    resultado = numerador / denominador
+
+    if resultado > 0 and resultado.is_integer():
+        print("Resultado:", int(resultado))  
+    elif resultado != 0:
+        print("Resultado:", f"{numerador}/{denominador}")
+    break
---
+++ estatistica/questao6.py
@@ -0,0 +1,25 @@
+aC=int(input("Quantas cervejas a pessoa A comprou? ")) # pessoa A cerveja
+aE=int(input("Quantos espetos a pessoa A comprou? ")) # pessoa A espeto
+aR=int(input("Quantos refrigerantes a pessoa A comprou? ")) # pessoa A refrigerante
+bC=int(input("Quantas cervejas a pessoa B comprou? ")) # pessoa B 
+bE=int(input("Quantos espetos a pessoa B comprou? ")) 
+bR=int(input("Quantos refrigerantes a pessoa B comprou? ")) 
+cC=int(input("Quantas cervejas a pessoa C comprou? ")) # pessoa C 
+cE=int(input("Quantos espetos a pessoa C comprou? ")) 
+cR=int(input("Quantos refrigerantes a pessoa C comprou? "))
+dC=int(input("Quantas cervejas a pessoa D comprou? ")) # pessoa D
+dE=int(input("Quantos espetos a pessoa D comprou? ")) 
+dR=int(input("Quantos refrigerantes a pessoa D comprou? "))
+eC=int(input("Quantas cervejas a pessoa E comprou? ")) # pessoa E
+eE=int(input("Quantos espetos a pessoa E comprou? ")) 
+eR=int(input("Quantos refrigerantes a pessoa E comprou? "))
+
+#cerveja6,50 espeto 8 e refrigerante 4,50
+
+aT= (aC * 6.5) + (aE * 8) + (aR * 4.5) # pessoa A total
+bT= (bC * 6.5) + (bE * 8) + (bR * 4.5) # pessoa B 
+cT= (cC * 6.5) + (cE * 8) + (cR * 4.5) # pessoa C 
+dT= (dC * 6.5) + (dE * 8) + (dR * 4.5) # pessoa D
+eT= (eC * 6.5) + (eE * 8) + (eR * 4.5) # pessoa E 
+
+print(f"O valor total gasto pela pessoa A foi: R${aT}" f"\nO valor total gasto pela pessoa B foi: R${bT}" f"\nO valor total gasto pela pessoa C foi: R${cT}" f"\nO valor total gasto pela pessoa D foi: R${dT}" f"\nO valor total gasto pela pessoa E foi: R${eT}" )
